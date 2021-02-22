7d98a211b63f95a383f53ad0e7a39b1c5b42b098
purisai <75816325+purisai@users.noreply.github.com>
2021-02-21 20:39:27 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Cracker-Sandwiches
+# angryBirdsStage2
+This makes really yummy cracker sandwiches
+You will need crackers, cheese, sourcream, tomato and beans
+You will cook the beans 
+Then you will cut the tomatos
+then you will take two crackers and put them on a plate
+After that, you will put a slice of cheese on one side of the cracker and put a slice of tomato on top of that
+Then you will spread very little sourcream on top of all that and then top it with a spoonful of beans
+Then put the other cracker on top of that
+You are done and can enjoy this really good cracker sandwich
---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+
+function setup(){
+    var canvas = createCanvas(400,400);
+
+}
+
+function draw(){
+    Text("How to make the greatest cracker sandwiches ever!", 200, 20)
+    Text("Ingredients", 150, 50)
+    Text("Ritz crackers", 150, 70 )
+    Text("Canned Beans", 150,90)
+    Text("tomatos", 150,110)
+    Text("sour cream", 150,130)
+    Text("cheese", 150,150)
+    Text("steps", 350, 50)
+    Text("1.You will cook the beans", 350, 70)
+    Text("2. Then you will cut the tomatos", 350, 90)
+    Text("3.then you will take two crackers and put them on a plate", 350, 110)
+    Text("4. After that, you will put a slice of cheese on one side of the cracker", 350,130)
+    Text("5.Put a slice of tomato on top of that",350,150)
+    Text("6.Then you will spread very little sourcream on top of all that", 350, 170)
+    Text("7.Then top it with a spoonful of beans",350,190)
+    Text("8.Then put the other cracker on top of that")
+    Text("Enjoy", 200,300)
+}
